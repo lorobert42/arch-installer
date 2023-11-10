32b0c8960952935bfd0508318d51869177520a79
lorobert <lorobert@student.42lausanne.ch>
2023-11-10 10:54:23 +0100

    Switch to Xorg
    Better for VM testing

--- setup.sh
+++ setup.sh
@@ -10,17 +10,8 @@
 # Define the software that would be inbstalled 
 #Need some prep work
 prep_stage=(
-    qt5-wayland 
-    qt5ct
-    qt6-wayland 
-    qt6ct
-    qt5-svg
-    qt5-quickcontrols2
-    qt5-graphicaleffects
-    gtk3 
-    polkit-gnome 
-    wl-clipboard 
-    cliphist 
+		xorg
+		xorg-xinit
     pacman-contrib
 )
 
@@ -35,13 +26,17 @@ nvidia_stage=(
 
 #the main packages
 install_stage=(
-    foot 
-    waybar
-    xdg-desktop-portal 
+		i3-wm
+		i3status
+		i3blocks
+    alacritty 
     firefox
-    network-manager-applet 
+		networkmanager
     ttf-jetbrains-mono-nerd 
+		unzip
+		chezmoi
     sddm
+		zsh
 )
 
 for str in ${myArray[@]}; do
@@ -96,7 +91,7 @@ install_software() {
 clear
 
 # set some expectations for the user
-echo -e "$CNT - You are about to execute a script that would attempt to setup Sway."
+echo -e "$CNT - You are about to execute a script that would attempt to setup i3."
 sleep 1
 
 # attempt to discover if this is a VM or not
@@ -175,8 +170,6 @@ if [ ! -f /sbin/paru ]; then
     fi
 fi
 
-
-
 ### Install all of the above pacakges ####
 read -rep $'[\e[1;33mACTION\e[0m] - Would you like to install the packages? (y,n) ' INST
 if [[ $INST == "Y" || $INST == "y" ]]; then
@@ -200,18 +193,6 @@ if [[ $INST == "Y" || $INST == "y" ]]; then
         echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
     fi
 
-    # Install the correct hyprland version
-    echo -e "$CNT - Installing Sway, this may take a while..."
-    if [[ "$ISNVIDIA" == true ]]; then
-        #check for sway and remove it so the -nvidia package can be installed
-        if paru -Q sway &>> /dev/null ; then
-            paru -R --noconfirm sway &>> $INSTLOG &
-        fi
-        install_software sway-nvidia
-    else
-        install_software sway
-    fi
-
     # Stage 1 - main components
     echo -e "$CNT - Installing main components, this may take a while..."
     for SOFTWR in ${install_stage[@]}; do
@@ -227,130 +208,28 @@ if [[ $INST == "Y" || $INST == "y" ]]; then
     echo -e "$CNT - Enabling the SDDM Service..."
     sudo systemctl enable sddm &>> $INSTLOG
     sleep 2
-    
-    # Clean out other portals
-    echo -e "$CNT - Cleaning out conflicting xdg portals..."
-    paru -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk &>> $INSTLOG
 fi
 
-# ### Copy Config Files ###
-# read -rep $'[\e[1;33mACTION\e[0m] - Would you like to copy config files? (y,n) ' CFG
-# if [[ $CFG == "Y" || $CFG == "y" ]]; then
-#     echo -e "$CNT - Copying config files..."
-#
-#     # copy the conf directory
-#     cp -R conf ~/.config/
-#
-#     #set the measuring unit
-#     echo -e "$CNT - Attempring to set mesuring unit..."
-#     if locale -a | grep -q ^en_US; then
-#         echo -e "$COK - Setting mesuring system to imperial..."
-#         ln -sf ~/.config/HyprV/waybar/conf/mesu-imp.jsonc ~/.config/HyprV/waybar/conf/mesu.jsonc
-#         sed -i 's/SET_MESU=""/SET_MESU="I"/' ~/.config/HyprV/hyprv.conf
-#     else
-#         echo -e "$COK - Setting mesuring system to metric..."
-#         sed -i 's/SET_MESU=""/SET_MESU="M"/' ~/.config/HyprV/hyprv.conf
-#         ln -sf ~/.config/HyprV/waybar/conf/mesu-met.jsonc ~/.config/HyprV/waybar/conf/mesu.jsonc
-#     fi
-#
-#     # Setup each appliaction
-#     # check for existing config folders and backup 
-#     for DIR in hypr kitty mako swaylock waybar wlogout wofi 
-#     do 
-#         DIRPATH=~/.config/$DIR
-#         if [ -d "$DIRPATH" ]; then 
-#             echo -e "$CAT - Config for $DIR located, backing up."
-#             mv $DIRPATH $DIRPATH-back &>> $INSTLOG
-#             echo -e "$COK - Backed up $DIR to $DIRPATH-back."
-#         fi
-#
-#         # make new empty folders
-#         mkdir -p $DIRPATH &>> $INSTLOG
-#     done
-#
-#     # link up the config files
-#     echo -e "$CNT - Setting up the new config..." 
-#     cp ~/.config/HyprV/hypr/* ~/.config/hypr/
-#     ln -sf ~/.config/HyprV/kitty/kitty.conf ~/.config/kitty/kitty.conf
-#     ln -sf ~/.config/HyprV/mako/conf/config-dark ~/.config/mako/config
-#     ln -sf ~/.config/HyprV/swaylock/config ~/.config/swaylock/config
-#     ln -sf ~/.config/HyprV/waybar/conf/v4-config.jsonc ~/.config/waybar/config.jsonc
-#     ln -sf ~/.config/HyprV/waybar/style/v4-style-dark.css ~/.config/waybar/style.css
-#     ln -sf ~/.config/HyprV/wlogout/layout ~/.config/wlogout/layout
-#     ln -sf ~/.config/HyprV/wofi/config ~/.config/wofi/config
-#     ln -sf ~/.config/HyprV/wofi/style/v4-style-dark.css ~/.config/wofi/style.css
-#
-#
-#     # add the Nvidia env file to the config (if needed)
-#     if [[ "$ISNVIDIA" == true ]]; then
-#         echo -e "\nsource = ~/.config/hypr/env_var_nvidia.conf" >> ~/.config/hypr/hyprland.conf
-#     fi
-#
-#     # Copy the SDDM theme
-#     echo -e "$CNT - Setting up the login screen."
-#     sudo cp -R Extras/sdt /usr/share/sddm/themes/
-#     sudo chown -R $USER:$USER /usr/share/sddm/themes/sdt
-#     sudo mkdir /etc/sddm.conf.d
-#     echo -e "[Theme]\nCurrent=sdt" | sudo tee -a /etc/sddm.conf.d/10-theme.conf &>> $INSTLOG
-#     WLDIR=/usr/share/wayland-sessions
-#     if [ -d "$WLDIR" ]; then
-#         echo -e "$COK - $WLDIR found"
-#     else
-#         echo -e "$CWR - $WLDIR NOT found, creating..."
-#         sudo mkdir $WLDIR
-#     fi 
-#     
-#     # stage the .desktop file
-#     sudo cp Extras/hyprland.desktop /usr/share/wayland-sessions/
-#
-#     # setup the first look and feel as dark
-#     xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita-dark"
-#     xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark"
-#     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-#     gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
-#     cp -f ~/.config/HyprV/backgrounds/v4-background-dark.jpg /usr/share/sddm/themes/sdt/wallpaper.jpg
-# fi
-#
-# ### Install the starship shell ###
-# read -rep $'[\e[1;33mACTION\e[0m] - Would you like to activate the starship shell? (y,n) ' STAR
-# if [[ $STAR == "Y" || $STAR == "y" ]]; then
-#     # install the starship shell
-#     echo -e "$CNT - Hansen Crusher, Engage!"
-#     echo -e "$CNT - Updating .bashrc..."
-#     echo -e '\neval "$(starship init bash)"' >> ~/.bashrc
-#     echo -e "$CNT - copying starship config file to ~/.config ..."
-#     cp Extras/starship.toml ~/.config/
-# fi
-#
-# ### Install software for Asus ROG laptops ###
-# read -rep $'[\e[1;33mACTION\e[0m] - For ASUS ROG Laptops - Would you like to install Asus ROG software support? (y,n) ' ROG
-# if [[ $ROG == "Y" || $ROG == "y" ]]; then
-#     echo -e "$CNT - Adding Keys..."
-#     sudo pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35 &>> $INSTLOG
-#     sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35 &>> $INSTLOG
-#     sudo pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35 &>> $INSTLOG
-#     sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35 &>> $INSTLOG
-#
-#     LOC="/etc/pacman.conf"
-#     echo -e "$CNT - Updating $LOC with g14 repo."
-#     echo -e "\n[g14]\nServer = https://arch.asus-linux.org" | sudo tee -a $LOC &>> $INSTLOG
-#     echo -e "$CNT - Update the system..."
-#     sudo pacman -Suy --noconfirm &>> $INSTLOG
-#
-#     echo -e "$CNT - Installing ROG pacakges..."
-#     install_software asusctl
-#     install_software supergfxctl
-#     install_software rog-control-center
-#
-#     echo -e "$CNT - Activating ROG services..."
-#     sudo systemctl enable --now power-profiles-daemon.service &>> $INSTLOG
-#     sleep 2
-#     sudo systemctl enable --now supergfxd &>> $INSTLOG
-#     sleep 2
-#
-#     # add the ROG keybinding file to the config
-#     echo -e "\nsource = ~/.config/hypr/rog-g15-strix-2021-binds.conf" >> ~/.config/hypr/hyprland.conf
-# fi
+### Copy Config Files ###
+read -rep $'[\e[1;33mACTION\e[0m] - Would you like to copy config files? (y,n) ' CFG
+if [[ $CFG == "Y" || $CFG == "y" ]]; then
+    echo -e "$CNT - Copying config files..."
+
+		# Install OMZ
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+		# Install OMP
+		curl -s https://ohmyposh.dev/install.sh | sudo bash -s
+
+		# Apply chezmoi
+		chezmoi --init --apply lorobert42
+
+    # Copy the SDDM theme
+    echo -e "$CNT - Setting up the login screen."
+    sudo cp -R /home/$USER/.config/sddm/catppuccin-mocha /usr/share/sddm/themes/
+    sudo chown -R $USER:$USER /usr/share/sddm/themes/catppuccin-mocha
+    sudo mkdir -p /etc/sddm.conf.d
+		sudo cp /home/$USER/.config/sddm/sddm.conf /etc/sddm.conf.d/
 
 ### Script is done ###
 echo -e "$CNT - Script had completed!"
@@ -360,8 +239,8 @@ if [[ "$ISNVIDIA" == true ]]; then
     exit
 fi
 
-read -rep $'[\e[1;33mACTION\e[0m] - Would you like to start Sway now? (y,n) ' HYP
-if [[ $HYP == "Y" || $HYP == "y" ]]; then
+read -rep $'[\e[1;33mACTION\e[0m] - Would you like to start i3 now? (y,n) ' I3
+if [[ $I3 == "Y" || $I3 == "y" ]]; then
     exec sudo systemctl start sddm &>> $INSTLOG
 else
     exit
